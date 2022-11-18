40b48762ebeb7416a9ac4b870dad44406aef5717
poorna <poorna@gmail.com>
2022-11-18 09:42:51 +0000

    pal.c is modified

--- pal.c
+++ pal.c
@@ -5,18 +5,18 @@ pallandrome() {
     scanf("%d", &n);
     original = n;
 
-    // reversed integer is stored in reversed variable
+    //// reversed integer is stored in reversed variable
     while (n != 0) {
         remainder = n % 10;
         reversed = reversed * 10 + remainder;
         n /= 10;
     }
 
-    // palindrome if orignal and reversed are equal
+   //// palindrome if orignal and reversed are equal
     if (original == reversed)
         printf("%d is a palindrome.", original);
     else
         printf("%d is not a palindrome.", original);
 
-    // return 0;
+    //// return 0;
 }
